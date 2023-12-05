f163530a6345b9d6a4a67e20f2e92d492cdc152e
nskhu12 <nskhu12@gmail.com>
2023-12-05 07:37:09 +0400

    implement Employee class

---
+++ ClassesAndObjectsTask/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassesAndObjectsTask
+{
+    public class Employee
+    {
+        private string surname;
+        private int age;
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(string surname, int age)
+        {
+            this.surname = surname ?? throw new ArgumentNullException(nameof(surname), $"{nameof(surname)} can not be null");
+            this.age = age;
+        }
+
+        public void SetSurname(string surname) => this.surname = surname;
+
+        public override string ToString()
+        {
+            return $"Surname: {this.surname}, Age: {age}";
+        }
+
+        private string GetAge()
+        {
+            return age.ToString();
+        }
+    }
+}
